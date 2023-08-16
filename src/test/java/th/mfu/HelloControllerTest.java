9157addfe257979735f61dbae6ecc96f8fc2d5ad
cnacha-mfu <81607567+cnacha-mfu@users.noreply.github.com>
2023-08-16 16:05:12 +0700

    add unitest

--- src/test/java/th/mfu/HelloControllerTest.java
+++ src/test/java/th/mfu/HelloControllerTest.java
@@ -1,23 +1,26 @@
 package th.mfu;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@WebMvcTest(HelloController.class)
 public class HelloControllerTest {
-
     @Autowired
-    private HelloController controller;
+    private MockMvc mockMvc;
 
-    // @Test
-    // public void testHello() {
-
-    //     // Act
-    //     String response = controller.hello();
-    //     // Assert
-    //     assertEquals("Hello World!", response);
-    // }
+    @Test
+    public void testHello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("greet"))
+                .andExpect(model().attribute("message", containsString("Welcome to Spring MVC!")));
+    }
+   
 }
